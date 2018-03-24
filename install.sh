87b8b40bcd5a913a9dcdcea27947272f3ea70ff9
ueno-t <uenotoshiyuki@gmail.com>
2018-03-24 11:37:50 +0900

    dotfiles: modify install.sh deploy and initialize

--- install.sh
+++ install.sh
@@ -7,18 +7,30 @@ if [ $? -ne 0 ]; then
     die "not found: $DOTPATH"
 fi
 
-for f in .??*
-do
+deploy() {
+  for f in .??*
+  do
     [ "$f" = ".git" ] && continue
 
     ln -snfv "$DOTPATH/$f" "$HOME/$f"
-done
+  done
+}
 
-# base
-sudo apt-get install -y git zsh vim tmux ctags curl
-# sudo apt-get install -y make binutils bison gcc build-essential
+initialize() {
+  # base
+  sudo apt-get install -y git zsh vim tmux ctags curl
+  sudo apt-get install -y make binutils bison gcc build-essential
 
-# vim dein
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ~/installer.sh
-sh ~/install.sh ~/.cache/dein
-rm ~/install.sh
+  # vim dein
+  curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > ~/installer.sh
+  sh ~/installer.sh ~/.cache/dein
+  rm ~/installer.sh
+}
+
+if [ "$1" = "deploy" -o "$1" = "d" ]; then
+  echo "deploy"
+  deploy
+elif [ "$1" = "init" -o "$1" = "i" ]; then
+  echo "initialize"
+  initialize
+fi
